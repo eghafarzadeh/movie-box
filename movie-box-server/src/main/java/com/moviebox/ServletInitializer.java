9e3a9080a94ef15ff8b2d1defa5acfdf936b251c
elham.ghafarzadeh <ghafarzade.elham@gmail.com>
2020-05-29 16:42:55 +0430

    creating project structure

---
+++ movie-box-server/src/main/java/com/moviebox/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.moviebox;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MovieboxApplication.class);
+	}
+
+}
---
+++ movie-box-server/src/test/java/com/moviebox/MovieboxApplicationTests.java
@@ -0,0 +1,14 @@
+package com.moviebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+		/* for future implementation*/
+	}
+
+}
